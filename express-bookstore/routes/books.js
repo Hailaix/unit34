618e54202694e6ee9027ca7c5b574b1f6d617a63
NHower <howernickoli@gmail.com>
2022-10-13 17:28:16 -0400

    book schema and validation tentatively complete

--- express-bookstore/routes/books.js
+++ express-bookstore/routes/books.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const { validate } = require("jsonschema");
+const ExpressError = require("../../express-messagely/expressError");
 const Book = require("../models/book");
+const bookSchema = require("../schema/bookSchema.json");
 
 const router = new express.Router();
+
 
 
 /** GET / => {books: [book, ...]}  */
@@ -30,6 +34,13 @@ router.get("/:id", async function (req, res, next) {
 
 router.post("/", async function (req, res, next) {
   try {
+    //validate the body
+    const validBody = validate(req.body, bookSchema);
+    //if the body does not validate, thow an error
+    if(! validBody.valid ){
+      const errors = validBody.errors.map(e => e.stack);
+      throw new ExpressError(errors, 400);
+    }
     const book = await Book.create(req.body);
     return res.status(201).json({ book });
   } catch (err) {
@@ -41,6 +52,13 @@ router.post("/", async function (req, res, next) {
 
 router.put("/:isbn", async function (req, res, next) {
   try {
+    //validate the body
+    const validBody = validate(req.body, bookSchema);
+    //if the body does not validate, thow an error
+    if(! validBody.valid ){
+      const errors = validBody.errors.map(e => e.stack);
+      throw new ExpressError(errors, 400);
+    }
     const book = await Book.update(req.params.isbn, req.body);
     return res.json({ book });
   } catch (err) {
